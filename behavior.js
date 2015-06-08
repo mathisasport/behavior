ae98eb612c1c5ef220ac3676e60680fa87c6af88
W. Scott Stornetta <stornetta@mathisasport.org>
2015-06-07 20:59:21 -0400

    new collection, behavior_events, and can specify which behavior occurred.

--- behavior.js
+++ behavior.js
@@ -1,14 +1,17 @@
 StudentList = new Mongo.Collection('students');
+BehaviorEvents = new Mongo.Collection('behavior_events');
 
 if (Meteor.isClient) {
+  Session.setDefault('selectedPeriod',0);
+  Session.setDefault('selectedStudent',false);
+  Session.setDefault('selectedBehavior',0);  
+  Session.setDefault('addBehavior',false);  
+
   Template.body.events({
 	    "submit .add_student": function (event) {
 		var name = event.target.student_name.value;
-		var student_id = 12345;
-		console.log('hey');
 		StudentList.insert({
 			    name: name,
-			    studentid: student_id,
 			    createdAt: new Date(),
 			    period: Session.get('selectedPeriod'),
 			    teacher: Meteor.userId(),
@@ -21,7 +24,23 @@ if (Meteor.isClient) {
 	    },
 	    "change select[name='section']": function(event){
 		Session.set("selectedPeriod",parseInt(event.target.value));	
+	    },
+	    "change select[name='behaviors']": function(event){
+		Session.set("selectedBehavior",parseInt(event.target.value));
+		if(Session.get('addBehavior') && 
+		   Session.get('selectedBehavior')!=1)
+		{
+		    BehaviorEvents.insert({    
+			student_id: Session.get('selectedStudent'), 
+			time: new Date(), 
+			action: Session.get('selectedBehavior')   
+		    });
+		    Session.set('addBehavior',false);
+		    Session.set('selectedBehavior',0);	
+		}    
+		Session.set('selectedStudent',false);
 	    }	
+	
 
       });	    
   Template.studentListing.helpers({
@@ -40,10 +59,9 @@ if (Meteor.isClient) {
 	      Session.set('selectedStudent',this._id);
 	      var new_score = this.score >= 10 ? this.score - 10: 0;
 	      StudentList.update(this._id, {$set: {score: new_score}});
+	      Session.set('addBehavior',true);
 	  }
       });
-
-  Session.setDefault('selectedPeriod',0);
 }
 
 if (Meteor.isServer) {
